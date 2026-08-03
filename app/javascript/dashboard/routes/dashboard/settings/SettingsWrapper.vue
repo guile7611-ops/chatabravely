9fe595ddda6e440a726fe68f0bfc967734ec53b8
Guilherme <seuemail@gmail.com>
2026-08-03 19:19:11 -0300

    fix(settings): render router-view directly in SettingsWrapper to fix navigation rendering

--- app/javascript/dashboard/routes/dashboard/settings/SettingsWrapper.vue
+++ app/javascript/dashboard/routes/dashboard/settings/SettingsWrapper.vue
@@ -16,12 +16,7 @@ const route = useRoute();
     class="flex flex-col w-full h-full m-0 pb-8 pt-4 px-6 overflow-auto bg-n-surface-1"
   >
     <div class="flex items-start w-full max-w-5xl mx-auto">
-      <router-view v-slot="{ Component }">
-        <keep-alive v-if="keepAlive">
-          <component :is="Component" :key="route.fullPath" />
-        </keep-alive>
-        <component :is="Component" v-else :key="route.fullPath" />
-      </router-view>
+      <router-view />
     </div>
   </div>
 </template>
